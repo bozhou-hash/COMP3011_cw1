c6d999a17159c76c33c8165bd340e985d99fd9c2
bozhou-hash <bz04cheng@gmail.com>
2026-03-16 23:53:21 +0000

    Updated models.py for authentication key implementation

--- api/models.py
+++ api/models.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+
+
 class Retailer(Base):
     __tablename__ = "retailers"
 
